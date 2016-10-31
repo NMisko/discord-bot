88e4f66f67cf967c08832914459d14069656c2e4
Nicola Miskowiec <nicola.miskowiec@gmail.com>
2016-11-01 00:16:41 +0100

    Jarvis lives again :)

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -203,7 +203,7 @@ func main() {
 
 	// Create a discord session
 	log.Info("Starting discord session...")
-	discord, err = discordgo.New(*Token)
+	discord, err = discordgo.New("Bot " + *Token)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
